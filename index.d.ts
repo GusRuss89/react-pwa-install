bdf9d72c88c628b9aa2a19e380968b8b9fcb8468
Angus Russell <angus.russell89@gmail.com>
2021-06-12 21:38:29 +1000

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 /**
  * A hook that provides:
  *  - supported
@@ -17,10 +19,18 @@ export function useReactPWAInstall(): {
   }) => Promise<void>;
 };
 
+type DialogProps = {
+  open: boolean,
+  onClose: () => void,
+  title?: string,
+  platform: string,
+  onSubmit: () => void
+}
+
 /**
  * Context provider for react-pwa-install
  *
  *  See: https://www.npmjs.com/package/react-pwa-install for more info.
  */
-export const ReactPWAInstallProvider: React.FC<{ enableLogging?: boolean }>;
+export const ReactPWAInstallProvider: React.FC<{ enableLogging?: boolean, dialogComponent: React.FC<DialogProps> }>;
 export default ReactPWAInstallProvider;
